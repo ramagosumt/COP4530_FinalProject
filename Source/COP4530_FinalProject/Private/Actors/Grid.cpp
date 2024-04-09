3c44918449180cd0c722c392ca3c9f5e675b43e0
Rama <nmt3@usf.edu>
2024-04-09 18:08:35 -0400

    Added GridLocation, GridSizeWorld, GridBoxColor, GridBottomLeftColor, GridTileNumberX, GridTileNumberY, GridBottomLeft(), GridTileNumber(), and DrawTile(). Clean Grid class.

--- Source/COP4530_FinalProject/Private/Actors/Grid.cpp
+++ Source/COP4530_FinalProject/Private/Actors/Grid.cpp
@@ -1,27 +1,89 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Actors/Grid.h"
+#include "Components/BillboardComponent.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values
 AGrid::AGrid()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a SceneComponent and set it as the RootComponent
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SetRootComponent(SceneComponent);
+
+	// Create a BillboardComponent and attach it to RootComponent
+	BillboardComponent = CreateDefaultSubobject<UBillboardComponent>(TEXT("BillboardComponent"));
+	BillboardComponent->SetupAttachment(GetRootComponent());
+
+	// Initialize Variables
+	GridLocation = FVector(0.f);
+	GridSizeWorld = FVector2D(500.f);
+	TileSize = 50.f;
+	GridTileNumberX = 0;
+	GridTileNumberY = 0;
+	GridBoxColor = FColor::Blue;
+	GridBottomLeftColor = FColor::Purple;
 }
 
-// Called when the game starts or when spawned
+void AGrid::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	FlushPersistentDebugLines(GetWorld());
+	
+	SetGridLocation(SceneComponent->GetComponentLocation());
+	
+	DrawDebugBox(GetWorld(), GridLocation, FVector(GridSizeWorld.Y, GridSizeWorld.X, 5.f), GridBoxColor, true, 1000000.f, 0.f, 10.f);
+	DrawDebugBox(GetWorld(), GridBottomLeft(), FVector(20.f), GridBottomLeftColor, true, 1000000.f, 0.f, 10.f);
+
+	GridTileNumber(GridTileNumberX, GridTileNumberY);
+
+	DrawTile();
+	
+}
+
 void AGrid::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AGrid::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
+FVector AGrid::GridBottomLeft() const
+{
+	// Movement = Move Direction * Move Length
+	const FVector MoveRight = SceneComponent->GetRightVector() * GridSizeWorld.X; // Move Horizontally
+	const FVector MoveUp = SceneComponent->GetForwardVector() * GridSizeWorld.Y; // Move Vertically
+	
+	const FVector BottomLeft = GridLocation - MoveRight - MoveUp;
+	
+	return BottomLeft;
+}
+
+void AGrid::GridTileNumber(int32& X, int32& Y) const 
+{
+	// Number of Tiles = Size of World / Size of Tiles
+	X = FMath::RoundToInt(GridSizeWorld.X / TileSize); // Horizontal Amount of Tiles
+	Y = FMath::RoundToInt(GridSizeWorld.Y / TileSize); // Vertical Amount of Tiles
+}
+
+void AGrid::DrawTile()
+{
+	for (int X = 0; X < GridTileNumberX; X++)
+	{
+		for (int Y = 0; Y < GridTileNumberY; Y++)
+		{
+			// Draw = Draw Direction * Draw Length
+			const FVector DrawRight = SceneComponent->GetRightVector() * (TileSize + X * TileSize * 2); // Draw Horizontally
+			const FVector DrawUp = SceneComponent->GetForwardVector() * (TileSize + Y * TileSize * 2); // Draw Vertically
+			
+			TilePosition = GridBottomLeft() + DrawRight + DrawUp;
+			
+			DrawDebugSolidPlane(GetWorld(), FPlane(0.f, 0.f, 1.f, TilePosition.Z), TilePosition, TileSize - 5.f, FColor::Yellow, true, 1000000.f);
+		}
+	}
+}
--- Source/COP4530_FinalProject/Public/Actors/Grid.h
+++ Source/COP4530_FinalProject/Public/Actors/Grid.h
@@ -1,26 +1,62 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Grid.generated.h"
 
+class UBillboardComponent;
+
 UCLASS()
 class COP4530_FINALPROJECT_API AGrid : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY()
+	USceneComponent* SceneComponent;
+
+	UPROPERTY()
+	UBillboardComponent* BillboardComponent;
 	
-public:	
-	// Sets default values for this actor's properties
+public:
+
+	UPROPERTY(EditInstanceOnly, Category= "Grid")
+	FVector GridLocation; // Map Center Location
+
+	UPROPERTY(EditInstanceOnly, Category= "Grid")
+	FVector2D GridSizeWorld; // Map Size
+
+	UPROPERTY(EditInstanceOnly, Category= "Grid")
+	float TileSize; // Tile Size
+
+	UPROPERTY(VisibleInstanceOnly, Category= "Grid")
+	int32 GridTileNumberX; // Number of Horizontal Tiles
+
+	UPROPERTY(VisibleInstanceOnly, Category= "Grid")
+	int32 GridTileNumberY; // Number of Vertical Tiles
+
+	UPROPERTY(EditInstanceOnly, Category= "Grid")
+	FVector TilePosition; // Position of Tiles
+
+	UPROPERTY(EditInstanceOnly, Category= "Debug")
+	FColor GridBoxColor;
+
+	UPROPERTY(EditInstanceOnly, Category= "Debug")
+	FColor GridBottomLeftColor;
+	
 	AGrid();
+	
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void Tick(float DeltaTime) override;
+	
+	FORCEINLINE FVector GetGridLocation() const { return GridLocation;};
+	FORCEINLINE void SetGridLocation(const FVector NewGridLocation) { GridLocation = NewGridLocation; } 
 
 protected:
-	// Called when the game starts or when spawned
+	
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	FVector GridBottomLeft() const; // Calculate the Position of BottomLeft
+	void GridTileNumber(int32& X, int32& Y) const; // Calculate the Amount of Tiles
 
+	void DrawTile(); // Draw the Tiles
 };
