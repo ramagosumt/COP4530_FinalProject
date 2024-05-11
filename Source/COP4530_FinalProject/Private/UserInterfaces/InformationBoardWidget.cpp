2f0c6b176eafbba8e3e853188b3aa85a570d2a53
ramagosumt <111352889+ramagosumt@users.noreply.github.com>
2024-05-11 17:01:03 -0400

    Updated InformationBoardWidget.

--- Source/COP4530_FinalProject/Private/UserInterfaces/InformationBoardWidget.cpp
+++ Source/COP4530_FinalProject/Private/UserInterfaces/InformationBoardWidget.cpp
@@ -18,13 +18,21 @@ void UInformationBoardWidget::NativeTick(const FGeometry& MyGeometry, float InDe
 		switch (GameMode)
 		{
 		case EGameMode::EGM_DFS:
+			ApplicationBoard->SetText(FText::FromString(
+				"- Topological Sorting, Scheduling Problems, Cycle Detection in Graphs\n\n"
+				"- Solving Puzzles with Only one Solution (Maze, Sudoku)\n\n"
+				"- Analyzing Networks (Testing if a Graph is bipartite)\n\n"
+				"- Subroutine in Network Flow Algorithms (Ford-Fulkerson Algorithm)\n\n"
+				"- Subroutine in Matching Algorithms in Graph Theory (Hopcroft-Karp Algorithm)\n\n"
+				"- Mapping Routes, Scheduling, and Finding Spanning Trees\n\n"
+			));
 			break;
 		case EGameMode::EGM_BFS:
 			ApplicationBoard->SetText(FText::FromString(
-				"- Solve games where a series of choices result in either winning or losing state\n\n"
+				"- Solve games where a series of choices result in either winning or losing state (Rubik)\n\n"
 				"- Dijkstra's Algorithm: Computing the shortest path in a graph\n\n"
 				"- Ford-Fulkerson Algorithm: Computing the maximum flow in a network\n\n"
-				));
+			));
 			break;
 		case EGameMode::EGM_Dijkstra:
 			break;
@@ -43,14 +51,21 @@ void UInformationBoardWidget::SetInformation() const
 		switch (GameMode)
 		{
 		case EGameMode::EGM_DFS:
-			UE_LOG(LogTemp, Warning, TEXT("Duh."))
+			ApplicationBoard->SetText(FText::FromString(
+				"- Topological Sorting, Scheduling Problems, Cycle Detection in Graphs\n\n"
+				"- Solving Puzzles with Only one Solution (Maze, Sudoku)\n\n"
+				"- Analyzing Networks (Testing if a Graph is bipartite)\n\n"
+				"- Subroutine in Network Flow Algorithms (Ford-Fulkerson Algorithm)\n\n"
+				"- Subroutine in Matching Algorithms in Graph Theory (Hopcroft-Karp Algorithm)\n\n"
+				"- Mapping Routes, Scheduling, and Finding Spanning Trees\n\n"
+			));
 			break;
 		case EGameMode::EGM_BFS:
 			ApplicationBoard->SetText(FText::FromString(
-				"- Solve games where a series of choices result in either winning or losing state\n\n"
+				"- Solve games where a series of choices result in either winning or losing state (Rubik)\n\n"
 				"- Dijkstra's Algorithm: Computing the shortest path in a graph\n\n"
 				"- Ford-Fulkerson Algorithm: Computing the maximum flow in a network\n\n"
-				));
+			));
 			break;
 		case EGameMode::EGM_Dijkstra:
 			break;
